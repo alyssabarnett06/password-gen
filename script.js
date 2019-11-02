6d109e99ce665af5b64bc6fa95e29297fe1f6682
Catherine Barnett <ca.barnett06@gmail.com>
2019-11-02 13:22:34 -0400

    added variables for prompts

---
+++ script.js
@@ -0,0 +1,56 @@
+var passQ = confirm("Would you like to generate a strong, random password?");
+var passAmt = "How many characters would you like your password to contain? ***Must be between 8 and 25 characters***";
+var passSpec = "Would you like your password to contain special characters?";
+var passNum = "Would you like your password to contain numeric characters?";
+var passLow = "Would you like your password to contain lowercase characters?";
+var passUpp = "Would you like your password to contain uppercase characters?";
+var noPass = "No password for you!";
+
+var charAmount;
+var specChar;
+var numCar;
+var lowerChar;
+var uppChar;
+
+
+
+
+
+
+
+// //generate random password
+// function generate() {
+
+
+//     //set password length/complexity
+//     let complexity= document.getElementById("slider").value;
+
+//     let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+
+//     let password= "";
+
+//     for(var i = 0; i <= complexity; i++){
+//         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+
+//     }
+
+//     //add password to text box
+
+// ;document.getElementById("display").value = password
+
+
+
+// }
+// document.getElementById("length").innerHTML="Length: 10";
+
+// //function to set slider length
+// document.getElementById("slider").oninput = function(){
+//     if(document.getElementById("slider").value >0){
+//         document.getElementById("length").innerHTML = "Length:" + document.getElementById("slider").value;
+
+//     }
+//     else{
+//         document.getElementById("length").innerHTML = "Length: 1";
+
+//     }
+// }
